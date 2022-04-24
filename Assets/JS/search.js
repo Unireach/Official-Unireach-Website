8d1be029594e78c97e9cf2dd0931dd80f5990150
Flame48 <advait.s.rajan@gmail.com>
2022-04-24 12:20:08 +0400

    Added functionality to activity search

--- Assets/JS/search.js
+++ Assets/JS/search.js
@@ -1,10 +1,14 @@
 //This javascript is used for searching through the activities and filtering out searches.
 const activitiesList = document.getElementById('activities');
+const searchBar = document.getElementById('searchInput');
+const SUPW_Btn = document.getElementById('SUPW_Btn');
+const CAS_Btn = document.getElementById('CAS_Btn');
+
 
 //List of objects containing all the information about the activity
 const activities = [
     {
-        title: 'Activity 1',
+        title: 'Green Cycle Drive',
         description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique harum tenetur velit nam amet, dolores, voluptate maiores qui esse culpa quam pariatur deserunt, quibusdam cum sit fuga! Amet, nulla qui?',
         imgUrl: '#',
         signUpLink: '#',
@@ -13,7 +17,7 @@ const activities = [
         SignUpActive: true,
     },
     {
-        title: 'Activity 2',
+        title: 'E-Waste',
         description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique harum tenetur velit nam amet, dolores, voluptate maiores qui esse culpa quam pariatur deserunt, quibusdam cum sit fuga! Amet, nulla qui?',
         imgUrl: '#',
         signUpLink: '#',
@@ -22,7 +26,7 @@ const activities = [
         SignUpActive: false,
     },
     {
-        title: 'Activity 3',
+        title: 'Unireach',
         description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique harum tenetur velit nam amet, dolores, voluptate maiores qui esse culpa quam pariatur deserunt, quibusdam cum sit fuga! Amet, nulla qui?',
         imgUrl: '#',
         signUpLink: '#',
@@ -30,7 +34,53 @@ const activities = [
         CAS: false,
         SignUpActive: true,
     },
+    {
+        title: 'Volunteer Work',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique harum tenetur velit nam amet, dolores, voluptate maiores qui esse culpa quam pariatur deserunt, quibusdam cum sit fuga! Amet, nulla qui?',
+        imgUrl: '#',
+        signUpLink: '#',
+        SUPW: true,
+        CAS: true,
+        SignUpActive: true,
+    },
 ];
+
+let filters = {
+    SUPW: false,
+    CAS: false,
+    SearchQuery: '',
+};
+
+searchBar.addEventListener('keyup', (e) => {
+    console.log(e.target.value);
+    filters.SearchQuery = e.target.value;
+    filterResults();
+});
+
+SUPW_Btn.addEventListener('change', (e) => {
+    console.log(e);
+    filters.SUPW = e.target.checked;
+    filterResults();
+});
+
+CAS_Btn.addEventListener('change', (e) => {
+    console.log(e);
+    filters.CAS = e.target.checked;
+    filterResults();
+});
+
+const filterResults = () => {
+    let filteredResults = activities.filter(activity => {
+        if((filters.SUPW && !activity.SUPW)||(filters.CAS && !activity.CAS)) {
+            return false;
+        }
+        if(filters.SearchQuery!='') {
+            return activity.title.toLowerCase().includes(filters.SearchQuery.toLowerCase());
+        }
+        return true;
+    });
+    displayActivities(filteredResults);
+}
 
 //Displays the activity
 const displayActivities = (list) => {
@@ -48,9 +98,13 @@ const displayActivities = (list) => {
             <div class="description">
                 ${activity.description}
             </div>
-            <div class="signUp">
-                <a href="${activity.signUpLink}" class="signUpBtn">Sign Up</a>
-            </div>
+            ${
+                //creates sign up button only if sign up is active
+                (activity.SignUpActive)?
+                `<div class="signUp">
+                    <a href="${activity.signUpLink}" class="signUpBtn">Sign Up</a>
+                </div>`:``
+            }
         </div>
         `;
     }).join('');
